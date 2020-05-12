75571e13054e01376b0f5a10a74bbb37733f64e8
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-12 21:40:28 +0530

    usecase1 added

--- src/com/Employeewage/controller/EmployeeWage.java
+++ src/com/Employeewage/controller/EmployeeWage.java
@@ -1,6 +1,22 @@
+
+
 class EmployeeWage{
+        static int isPresent=1;
+	public static double random(){
+	double status=Math.floor(Math.random()*10)%2;
+	return status;
+	}
+	public static void EmployeeAttendance()
+	{
+		if( random()==isPresent)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is absent");
+	}
 
 	public static void main (String[] args){
 		System.out.println("welcome to employee wage calculation program");
+		EmployeeWage empWage = new EmployeeWage();
+		empWage.EmployeeAttendance();
 	}
 }
